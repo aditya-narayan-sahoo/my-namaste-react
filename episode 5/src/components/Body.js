040da4a1a4c9df4f9dfe9c996bc28a36b23dc366
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-07-31 20:35:44 +0530

    Add state variable and useState  hook

--- episode 5/src/components/Body.js
+++ episode 5/src/components/Body.js
@@ -1,11 +1,25 @@
 import ResturantCard from "./ResturantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
+
 const Body = () => {
+  let [list, setList] = useState(resList);
+
   return (
     <div className="body">
-      <div className="search">Search bar to make</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = list.filter((res) => res.info.avgRating > 4);
+            setList(filteredList);
+          }}
+        >
+          Top Rated Resturants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((resturant) => (
+        {list.map((resturant) => (
           <ResturantCard key={resturant.info.id} resData={resturant} />
         ))}
       </div>
