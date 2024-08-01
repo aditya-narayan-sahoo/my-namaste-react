c820ece9b79dc783b04a1850b2b862f8969fa292
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-08-01 21:50:44 +0530

    Add Search Bar and Filteration

--- episode 6/src/components/Body.js
+++ episode 6/src/components/Body.js
@@ -1,42 +1,77 @@
 import ResturantCard from "./ResturantCard";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
-
+import resList from "../utils/mockData";
 const Body = () => {
-  let [list, setList] = useState([]);
+  const [filterText, setFilterText] = useState("4* and Above Resturants");
+  const [myList, setMyList] = useState(resList);
+  const [searchText, setSearchText] = useState("");
+  const [filteredList, setFilteredList] = useState(resList);
+
+  // Code to use Live API, beware of API not respnoding
+  /*
+  const [list, setList] = useState([]);
   useEffect(() => {
     fetchData();
   }, []);
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/mapi/homepage/getCards?lat=20.27060&lng=85.83340"
+      "https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=20.27060&lng=85.83340&carousel=true&third_party_vendor=1"
     );
     const jsonData = await data.json();
-    console.log(jsonData);
     setList(
-      jsonData?.data?.success?.cards[3]?.gridWidget?.gridElements?.infoWithStyle
-        .restaurants
+      jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
     );
-  };
+  }; */
 
-  if (list.length === 0) {
-    return <Shimmer />;
-  }
-  return (
+  return myList.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              const singleFiltered = myList.filter((resuno) =>
+                resuno.info.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase())
+              );
+              setFilteredList(singleFiltered);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
-            const filteredList = list.filter((res) => res.info.avgRating > 4);
-            setList(filteredList);
+            if (filterText === "4* and Above Resturants") {
+              const myFilteredList = myList.filter(
+                (res) => res.info.avgRating > 4
+              );
+              setMyList(myFilteredList);
+              setFilteredList(myFilteredList);
+              setFilterText("All Resturants");
+            } else {
+              setMyList(resList);
+              setFilteredList(resList);
+              setFilterText("4* and Above Resturants");
+            }
           }}
         >
-          Top Rated Resturants
+          {filterText}
         </button>
       </div>
       <div className="res-container">
-        {list.map((resturant) => (
+        {filteredList.map((resturant) => (
           <ResturantCard key={resturant.info.id} resData={resturant} />
         ))}
       </div>
--- episode 6/src/components/Header.js
+++ episode 6/src/components/Header.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+  const [name, setName] = useState("Login");
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +14,14 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              name === "Login" ? setName("Logout") : setName("Login");
+            }}
+          >
+            {name}
+          </button>
         </ul>
       </div>
     </div>
