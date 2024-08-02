da0c410fe06a1f064416f288ca52ea85560b43db
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-08-02 20:49:57 +0530

    Add Various Routes

--- episode 7/src/components/Body.js
+++ episode 7/src/components/Body.js
@@ -4,24 +4,24 @@ import Shimmer from "./Shimmer";
 const Body = () => {
   const [searchText, setSearchText] = useState("");
   const [filteredList, setFilteredList] = useState([]);
-
-  // Code to use Live API, beware of API not respnoding
+  const [isTopRated, setIsTopRated] = useState(false);
+  // Code to use Live API, take care of API not respnoding
   const [list, setList] = useState([]);
   useEffect(() => {
     fetchData();
   }, []);
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/mapi/homepage/getCards?lat=20.27060&lng=85.83340"
+      "https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING"
     );
     const jsonData = await data.json();
-    console.log(jsonData);
+    //console.log(jsonData);
     setList(
-      jsonData?.data?.success?.cards[3]?.gridWidget?.gridElements?.infoWithStyle
+      jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants
     );
     setFilteredList(
-      jsonData?.data?.success?.cards[3]?.gridWidget?.gridElements?.infoWithStyle
+      jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants
     );
   };
@@ -54,10 +54,15 @@ const Body = () => {
         <button
           className="filter-btn"
           onClick={() => {
-            // * Filter logic
-            const filteredList = list.filter((res) => res.info.avgRating > 4);
-            setFilteredList(filteredList);
-            console.log(filteredList);
+            if (isTopRated) {
+              // If already filtered, reset to show all restaurants
+              setFilteredList(list);
+            } else {
+              // Filter to show only top-rated restaurants
+              const topRatedList = list.filter((res) => res.info.avgRating > 4);
+              setFilteredList(topRatedList);
+            }
+            setIsTopRated(!isTopRated);
           }}
         >
           Top Rated Resturants
