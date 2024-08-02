e3fe0219c6c3e6241e09159b0e4633e90e7c3d73
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-08-02 14:19:30 +0530

    Using fetch API to get data

--- episode 7/src/components/Body.js
+++ episode 7/src/components/Body.js
@@ -1,31 +1,32 @@
 import ResturantCard from "./ResturantCard";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
-import resList from "../utils/mockData";
 const Body = () => {
-  const [filterText, setFilterText] = useState("4* and Above Resturants");
-  const [myList, setMyList] = useState(resList);
   const [searchText, setSearchText] = useState("");
-  const [filteredList, setFilteredList] = useState(resList);
+  const [filteredList, setFilteredList] = useState([]);
 
   // Code to use Live API, beware of API not respnoding
-  /*
   const [list, setList] = useState([]);
   useEffect(() => {
     fetchData();
   }, []);
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=20.27060&lng=85.83340&carousel=true&third_party_vendor=1"
+      "https://www.swiggy.com/mapi/homepage/getCards?lat=20.27060&lng=85.83340"
     );
     const jsonData = await data.json();
+    console.log(jsonData);
     setList(
-      jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+      jsonData?.data?.success?.cards[3]?.gridWidget?.gridElements?.infoWithStyle
         ?.restaurants
     );
-  }; */
+    setFilteredList(
+      jsonData?.data?.success?.cards[3]?.gridWidget?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  };
 
-  return myList.length === 0 ? (
+  return list.length === 0 ? (
     <Shimmer />
   ) : (
     <div className="body">
@@ -39,7 +40,7 @@ const Body = () => {
           />
           <button
             onClick={() => {
-              const singleFiltered = myList.filter((resuno) =>
+              const singleFiltered = list.filter((resuno) =>
                 resuno.info.name
                   .toLowerCase()
                   .includes(searchText.toLowerCase())
@@ -53,21 +54,13 @@ const Body = () => {
         <button
           className="filter-btn"
           onClick={() => {
-            if (filterText === "4* and Above Resturants") {
-              const myFilteredList = myList.filter(
-                (res) => res.info.avgRating > 4
-              );
-              setMyList(myFilteredList);
-              setFilteredList(myFilteredList);
-              setFilterText("All Resturants");
-            } else {
-              setMyList(resList);
-              setFilteredList(resList);
-              setFilterText("4* and Above Resturants");
-            }
+            // * Filter logic
+            const filteredList = list.filter((res) => res.info.avgRating > 4);
+            setFilteredList(filteredList);
+            console.log(filteredList);
           }}
         >
-          {filterText}
+          Top Rated Resturants
         </button>
       </div>
       <div className="res-container">
