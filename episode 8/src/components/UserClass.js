464e0bb81d8e9e3c61707a87242788cf8cfe22d0
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-08-13 12:38:07 +0530

    Add LifeCycle Method

--- episode 8/src/components/UserClass.js
+++ episode 8/src/components/UserClass.js
@@ -4,32 +4,48 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      //defining state variables in class component methods
-      count: 2.3,
-      count2: 23,
+      userInfo: {
+        name: "Noob",
+        location: "NoobCountry",
+      },
     };
   }
+  componentDidMount() {
+    fetch("https://api.github.com/users/aditya-narayan-sahoo")
+      .then((data) => data.json())
+      .then((response) => {
+        this.setState({
+          userInfo: response,
+        });
+        //console.log(response);
+      })
+      .catch((error) => {
+        console.error("Error fetching user data:", error);
+      });
+  }
+
+  /*
+    above promise chaining can be done using async await too like the below one
+  async componentDidMount() {
+    const data = await fetch(
+      "https://api.github.com/users/aditya-narayan-sahoo"
+    );
+    const response = await data.json();
+    this.setState({
+      userInfo: response,
+    });
+    console.log(response);
+  }
+    */
+
   render() {
-    const { name, location } = this.props;
-    const { count, count2 } = this.state;
+    const { name, location, avatar_url } = this.state.userInfo;
     return (
       <div>
         <h1>About</h1>
+        <img src={avatar_url} alt="Profile Picture" />
         <h2>Name : {name}</h2>
         <h2>Place : {location}</h2>
-        <h2>State Variable : {count}</h2>
-        <button
-          onClick={() => {
-            this.setState({
-              //updating the state variables in class components
-              count: this.state.count + 1,
-              count2: this.state.count2 + 1,
-            });
-          }}
-        >
-          Update State 1
-        </button>
-        <h2>State Variable : {count2}</h2>
       </div>
     );
   }
