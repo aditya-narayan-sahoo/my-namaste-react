cdd36f0947d18443099ebc0f20c821db05dc3141
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-08-16 00:53:39 +0530

    Add Obselete files

---
+++ episode 12/src/app.js
@@ -0,0 +1,50 @@
+import React, { lazy, Suspense, useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import RestaurantMenu from "./components/RestaurantMenu";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import UserContext from "./utils/UserContext";
+const About = lazy(() => import("./components/About"));
+const AppLayout = () => {
+  const [userName, setUserName] = useState("Default User");
+  // useEffect(() => {
+  //   //dummy data used instead of API CALL
+  //   const data = { name: "Aditya Narayan Sahoo" };
+  //   setUserName(data.name);
+  // }, []);
+  return (
+    <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
+      <div>
+        <Header />
+        <Outlet />
+      </div>
+    </UserContext.Provider>
+  );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Body /> },
+      {
+        path: "/about",
+        element: (
+          <Suspense fallback={<h1>About Page Loading...🔃🔃</h1>}>
+            <About />
+          </Suspense>
+        ),
+      },
+      { path: "/contact", element: <Contact /> },
+      { path: "/restaurant/:resId", element: <RestaurantMenu /> },
+    ],
+    errorElement: <Error />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={appRouter} />);
---
+++ episode 12/src/components/Header.js
@@ -0,0 +1,39 @@
+import { useState, useContext } from "react";
+import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
+import UserContext from "../utils/UserContext";
+const Header = () => {
+  const [name, setName] = useState("Login");
+  const { loggedInUser } = useContext(UserContext);
+  return (
+    <div className="flex justify-between">
+      <div className="logo-container">
+        <img className="w-36" src={LOGO_URL} alt="Company-Logo" />
+      </div>
+      <div className="flex items-center">
+        <ul className="flex p-4 m-4">
+          <li className="px-5">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="px-5">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="px-5">
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li className="px-5">Cart</li>
+          <button
+            className="px-5  bg-slate-300 p-1 -mt-1 rounded-md hover:bg-slate-400 hover:font-medium"
+            onClick={() => {
+              name === "Login" ? setName("Logout") : setName("Login");
+            }}
+          >
+            {name}
+          </button>
+          <li className="px-5">{loggedInUser}</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Header;
