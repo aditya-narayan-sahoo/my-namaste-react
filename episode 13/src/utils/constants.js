01e8f360213dd9780eb910cd4cbe97a31684d28b
Aditya Narayan Sahoo <88726679+aditya-narayan-sahoo@users.noreply.github.com>
2024-08-16 18:00:17 +0530

    Delete episode 13 directory

--- episode 13/src/utils/constants.js
+++
@@ -1,6 +0,0 @@
-export const CDN_URL =
-  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-export const LOGO_URL =
-  "https://penji.co/wp-content/uploads/2022/08/10.-mr.-d-food-logo.jpg";
-export const MENU_API_URL =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
